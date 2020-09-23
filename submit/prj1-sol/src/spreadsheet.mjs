862e6dd2a0f316612a924ed868e908a390c7b2bc
NAMAN GARG <namangarg@NAMANs-MacBook-Pro.local>
2020-09-24 03:16:15 +0530

    started project 1

--- submit/prj1-sol/src/spreadsheet.mjs
+++ submit/prj1-sol/src/spreadsheet.mjs
@@ -12,7 +12,9 @@ export default class Spreadsheet {
 
   constructor() {
     //@TODO
+      this.cell = new CellInfo();
   }
+    
 
   /** Set cell with id baseCellId to result of evaluating formula
    *  specified by the string expr.  Update all cells which are
@@ -26,10 +28,59 @@ export default class Spreadsheet {
   async eval(baseCellId, expr) {
     const updates = {};
     //@TODO
+	let ast= parse(expr, baseCellId);
+      let val = this.eval1(ast)
+      this.cell.id = baseCellId;
+      this.cell.expr = expr;
+      this.cell.value = val;
+      this.cell.dependents[baseCellId] = val;
+      this.cell.ast = ast;
+	console.log(inspect(ast, false, Infinity));
+      
+     
+     // updates.push({baseCellId: this.eval1(ast)});
+     console.log(baseCellId + ":" + val)
+      
+      
+      updates[baseCellId] = val;
     return updates;
+
   }
 
   //@TODO add methods
+   eval1(ast){
+	var retval= 0;
+       let refarray = [];
+	switch(ast.type){
+        case 'num' :
+            if(ast.fn !== null){
+                let str = ast.fn;
+                console.log(FNS.str);
+            }
+            else{
+                retval = retval + ast.value;
+            }
+        break;
+        
+        case 'app' :
+            
+            if (ast.type === 'num'){
+                    retArray.push(x.value);
+                }
+//                else if(x.type === 'ref'){
+//                    refdata = cellRefToCellId;
+
+               // }
+            
+            break;
+        
+        default:
+            retval= 0;
+	
+		}
+	return retval;
+
+	}
 }
 
 //Map fn property of Ast type === 'app' to corresponding function.
@@ -44,3 +95,69 @@ const FNS = {
 
 
 //@TODO add other classes, functions, constants etc as needed
+
+class CellInfo{
+    constructor(){
+    this.id = '';
+    this.expr = '';
+    this.value = 0;
+    this.dependents = {};
+    this.ast = null;
+}
+
+};
+
+class DAG {
+    
+    constructor(){
+        this.adj = new Map();
+    }
+    
+    addVertex(v){
+        this.adj.set(v,[]);
+    }
+    
+    addEdge(v,u){
+        this.adj.get(v).push(u);
+    }
+    
+    depthFirstSearch(node){
+        var visited = {};
+        this.dfsHelper(node,visited);
+        
+    }
+    
+    getAdjList(){
+        return adj;
+    }
+    
+    print(){
+        var dag = this.adj.keys();
+        for( var i in dag){
+            var val = this.adj.get(i);
+            var conc = "";
+            
+            for(var j in val){
+                conc += j + " ";
+            }
+            console.log(i + " -> " + conc);
+        }
+    }
+    
+    dfsHelper(node, visited){
+        visited[node] = true;
+        
+        var adjNode = this.adj.get(node);
+        
+        for(var i in adjNode){
+            var nNode = adjNode(i);
+            if(!visited[nNode]){
+                this.dfsHelper(nNode, visited);
+                
+            }
+            
+        }
+    }
+    
+    
+}
