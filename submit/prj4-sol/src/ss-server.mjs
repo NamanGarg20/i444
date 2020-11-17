74e208a23f763fd9b3ad1778fe565eecc614e9ce
NAMAN GARG <namangarg@NAMANs-MacBook-Pro.local>
2020-11-17 13:47:36 +0530

    git prh

--- submit/prj4-sol/src/ss-server.mjs
+++ submit/prj4-sol/src/ss-server.mjs
@@ -136,11 +136,12 @@ function doView(app){
         var ssTableValues = ssTable[1];
         for(var node of ssDump){
             var col = node[0].charCodeAt(0)-96;
-            console.log(node[0]);
+            console.log(col);
             console.log(node[0].substring(1));
             var row = parseInt(node[0].substring(1));
             var value = await spreadsheet.query(node[0]).value;
             ssTableValues[row][col] = value;
+            console.log(value);
         }
             
         ss_view['tableCol'] = ssTable[0];
